4805324324818932041d6381b3967ab58e48d2f6
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-02-25 20:22:25 +0530

    used added components within App

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+
+// styles
+import 'bootstrap/dist/css/bootstrap.css' // run `npm install bootstrap` to install and then import it
+
+// components
+import Alert from './components/Alert';
+import Button from './components/Button';
+// import ListGroup from './components/ListGroup';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [alertVisible, setAlertVisibility] = useState(false)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>Alert!</Alert>}
+      <Button type="primary" onClick={() => setAlertVisibility(true)}>Click Me!</Button>
     </div>
   )
 }
+
+// function App() {
+  // TypeScript is smart enough to infer the type of the items array
+  // however, if you want to be explicit, you can do so like this:
+  // let items: string[] = [...]
+  // const items = [
+  //     'Colombo',
+  //     'Galle',
+  //     'Jaffna',
+  //     'Kandy',
+  //     'Negombo'
+  // ]
+  // however, TypeScript requires us to be explicit about the type of the parameter in the situation below
+  // because it is not possible to infer the type of the event
+  // by including this annotation (it is required), we get autocompletion, type safety, and documentation
+  // const handleSelectItem = (item: string) => console.log(item);
+
+  // return <ListGroup items={items} heading='Cities' onSelectItem={handleSelectItem}></ListGroup>
+// }
 
 export default App
